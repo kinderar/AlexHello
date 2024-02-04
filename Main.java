ed56943bed3c8087af7ca4b8496a8d48b338c0f3
kinderar <156221770+kinderar@users.noreply.github.com>
2024-02-04 11:54:36 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        HelloGreeter greeter = new HelloGreeter("Hello from GitHub");
+        greeter.showRepeatGreeting(10);
+    }
+}
